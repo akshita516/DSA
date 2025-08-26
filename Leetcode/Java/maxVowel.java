2b78cbcfbe98980270a4070e2ccc20021ad50ac0
Akshita M B <akshitamb2512@gmail.com>
2025-08-26 10:44:09 +0530

    Create maxVowel.java

---
+++ Leetcode/Java/maxVowel.java
@@ -0,0 +1,23 @@
+/* Question : 1456. Maximum Number of Vowels in a Substring of Given Length */
+class Solution {
+    public int maxVowels(String s, int k) {
+        int left=0, n=s.length(), vowels=0, answer=0;
+        for(int right=0;right<n;right++){
+            /*if(right-left+1<=k && isVowel(s.charAt(right)))
+                vowels++;*/
+            while(right-left+1>k){
+                if(isVowel(s.charAt(left)))
+                    vowels--;
+                left++;
+            }
+            if(right-left+1<=k && isVowel(s.charAt(right)))
+                vowels++;
+            //System.out.println(s.substring(left,right+1));
+            answer=Math.max(answer,vowels);
+        }
+        return answer;
+    }
+    private boolean isVowel(char ch){
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+    }
+}
